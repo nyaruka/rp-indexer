02a8939f191add41850b61023d0f18311cce849c
Rowan Seymour <rowanseymour@gmail.com>
2019-11-22 14:41:19 -0500

    Use elasticsearch action from its own repo and remove no longer used vendor directory

--- .github/actions/elasticsearch/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker_run="docker run -d -p 9200:9200 -p 9300:9300 -e 'discovery.type=single-node' elasticsearch:$INPUT_ELASTIC_VERSION"
-
-echo "RUNNING: $docker_run"
-sh -c "$docker_run"

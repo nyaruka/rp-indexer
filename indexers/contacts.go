2d18991f4a06d38e8803cc051f1ca792d476eeb1
Rowan Seymour <rowanseymour@gmail.com>
2024-05-16 10:56:04 -0500

    Reduce batch size to 100,000

--- indexers/contacts.go
+++ indexers/contacts.go
@@ -150,7 +150,7 @@ SELECT org_id, id, modified_on, is_active, row_to_json(t) FROM (
 	FROM contacts_contact
 	WHERE modified_on >= $1
 	ORDER BY modified_on ASC
-	LIMIT 500000
+	LIMIT 100000
 ) t;
 `
 

eb2ee0f17270d70cf465520aea2712c922e24713
Norbert Kwizera <norkans7@gmail.com>
2026-03-05 16:36:39 +0200

    Remove v10

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/nyaruka/rp-indexer/v10
+module github.com/nyaruka/rp-indexer
 
 go 1.25
 
--- indexers/base_test.go
+++ indexers/base_test.go
@@ -16,8 +16,8 @@ import (
 	"github.com/nyaruka/gocommon/elastic"
 	"github.com/nyaruka/gocommon/httpx"
 	"github.com/nyaruka/gocommon/jsonx"
-	"github.com/nyaruka/rp-indexer/v10/indexers"
-	"github.com/nyaruka/rp-indexer/v10/runtime"
+	"github.com/nyaruka/rp-indexer/indexers"
+	"github.com/nyaruka/rp-indexer/runtime"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
